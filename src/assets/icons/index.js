3bd9d78d1fa94a1e821ec0d97d68dcf55bf130bc
MayowaObisesan <mayowaobi74@gmail.com>
2023-12-24 20:30:50 +0100

    Update summary page meta

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,8 @@
 const IconDefaults = {
     width: 24,
     height: 24,
+    viewWidth: 24,
+    viewHeight: 24,
     strokeWidth: 2
 }
 
@@ -60,6 +62,81 @@ export const HelpIconSquircle = ({ width = 24, height = 24, strokeWidth = 2 }) =
         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-help-square-rounded-filled" width={width} height={height} viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M12 2l.642 .005l.616 .017l.299 .013l.579 .034l.553 .046c4.687 .455 6.65 2.333 7.166 6.906l.03 .29l.046 .553l.041 .727l.006 .15l.017 .617l.005 .642l-.005 .642l-.017 .616l-.013 .299l-.034 .579l-.046 .553c-.455 4.687 -2.333 6.65 -6.906 7.166l-.29 .03l-.553 .046l-.727 .041l-.15 .006l-.617 .017l-.642 .005l-.642 -.005l-.616 -.017l-.299 -.013l-.579 -.034l-.553 -.046c-4.687 -.455 -6.65 -2.333 -7.166 -6.906l-.03 -.29l-.046 -.553l-.041 -.727l-.006 -.15l-.017 -.617l-.004 -.318v-.648l.004 -.318l.017 -.616l.013 -.299l.034 -.579l.046 -.553c.455 -4.687 2.333 -6.65 6.906 -7.166l.29 -.03l.553 -.046l.727 -.041l.15 -.006l.617 -.017c.21 -.003 .424 -.005 .642 -.005zm0 13a1 1 0 0 0 -.993 .883l-.007 .117l.007 .127a1 1 0 0 0 1.986 0l.007 -.117l-.007 -.127a1 1 0 0 0 -.993 -.883zm1.368 -6.673a2.98 2.98 0 0 0 -3.631 .728a1 1 0 0 0 1.44 1.383l.171 -.18a.98 .98 0 0 1 1.11 -.15a1 1 0 0 1 -.34 1.886l-.232 .012a1 1 0 0 0 .111 1.994a3 3 0 0 0 1.371 -5.673z" strokeWidth={0} fill="currentColor" />
+        </svg>
+    )
+}
+
+export const TwitterIcon = ({ width = 24, height = 24, viewWidth = 16, viewHeight = 16, strokeWidth = 2, classes = "" }) => {
+    return (
+        <svg
+            className={classes}
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height={height}
+            fill="currentColor"
+            viewBox={`0 0 ${viewWidth} ${viewHeight}`}
+            strokeWidth={strokeWidth}
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+        </svg>
+    )
+}
+
+export const GithubIcon = ({ width = 24, height = 24, viewWidth = 16, viewHeight = 16, strokeWidth = 2, classes = "" }) => {
+    return (
+        <svg
+            className={classes}
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height={height}
+            fill="currentColor"
+            viewBox={`0 0 ${viewWidth} ${viewHeight}`}
+            strokeWidth={strokeWidth}
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+        </svg>
+    )
+}
+
+export const LinkedIcon = ({ width = 24, height = 24, viewWidth = 16, viewHeight = 16, fill = "", strokeWidth = 2, classes = "" }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`icon icon-tabler icon-tabler-brand-linkedin ${classes}`}
+            width={width}
+            height={height}
+            viewBox={`0 0 ${viewWidth} ${viewHeight}`}
+            strokeWidth={strokeWidth}
+            stroke="white"
+            fill={"currentColor"}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+            <path d="M8 11l0 5" />
+            <path d="M8 8l0 .01" />
+            <path d="M12 16l0 -5" />
+            <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+        </svg>
+    )
+}
+export const MediumIcon = ({ width = 24, height = 24, viewWidth = 16, viewHeight = 16, fill = "", strokeWidth = 2, classes = "" }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-medium" width={24} height={24} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+            <path d="M8 9h1l3 3l3 -3h1" />
+            <path d="M8 15l2 0" />
+            <path d="M14 15l2 0" />
+            <path d="M9 9l0 6" />
+            <path d="M15 9l0 6" />
         </svg>
     )
 }
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,17 +1,4 @@
-import { Link, Outlet, ScrollRestoration } from "react-router-dom"
-// import { getSummaryTrends } from "./loaders"
-// import { DesktopNavBar, MobileNavBar } from "./NavBar";
-// import { useDeviceWidth } from "./useDeviceWidth";
-import { deviceWidthEnum } from "../helpers";
-import { DesktopNavBar, LaptopNavBar, MobileNavBar } from "../components/NavBar";
-import { useWindowSize } from "@uidotdev/usehooks";
-import { Home } from "./Home";
-import { useEffect, useRef, useState } from "react";
-import Summary from "./Summary";
-import { groupData } from "../helpers/loaders";
-import Footer from "../components/Footer";
-import { SummaryGradientText } from "../components/SummaryText";
-import { HelpContent } from "../components/HelpContent";
+import { Outlet, ScrollRestoration } from "react-router-dom"
 
 export async function loader() {
     // const { trends } = await getSummaryTrends();
@@ -20,119 +7,8 @@ export async function loader() {
 }
 
 export const Root = () => {
-    // const deviceWidth = useDeviceWidth();
-    const size = useWindowSize();
-    const [data, setData] = useState([]);
-    const [summaryData, setSummaryData] = useState([]);
-    const [resultArray, setResultArray] = useState([]);
-    const [resultGroupArray, setResultGroupArray] = useState([]);
-    // let resultArray = [];
-    // let resultGroupArray = [];
-    // let groupedData;
-
-    /*
-    useEffect(() => {
-        if (data) {
-            data.currentStartIndex = data.queries?.request[0]?.startIndex;
-            data.nextStartIndex = data.queries?.nextPage[0]?.startIndex;
-            // resultArray.push(data);
-            setResultArray((prev) => [...prev, data]);
-            groupedData = groupData(resultArray);
-            resultGroupArray.splice(0, resultGroupArray.length);
-            console.log(resultArray);
-            console.log(groupedData);
-            setResultGroupArray((v) => [...v, groupedData]);
-            // setData((res) => { })
-            console.log(resultGroupArray);
-            data.data = resultGroupArray;
-            console.log(data.data);
-
-            // setSummaryData(v => (
-            //     { ...v, data }
-            // ));
-        }
-        console.log(summaryData);
-        return () => null;
-    }, [data]);
-    */
-
-    // useEffect(() => {
-    //     setSummaryData(data);
-    // }, [summaryData]);
-
-    useEffect(() => {
-        setData(data);
-        console.log("Updating summaryData");
-        console.log(data);
-    }, [data]);
-
     return (
-        <div className={"relative flex flex-col h-full overflow-y-auto dark:bg-base-300 font-sans"}>
-            {
-                size?.width <= deviceWidthEnum.laptop
-                    ? <div className="drawer flex flex-col justify-start">
-                        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-                        <div className="drawer-content h-screen overflow-y-auto z-[1]">
-                            {/* Page content here */}
-                            <MobileNavBar setData={setData} />
-                            <Summary summary={data} />
-                            <Footer />
-                        </div>
-
-                        <div className="drawer-side z-10">
-                            <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                                {/* Sidebar content here */}
-                                <div className="px-3 py-6">Menu</div>
-                                <li><a>Bookmarks</a></li>
-                                <li><a>Saved</a></li>
-                                <div className="fixed left-0 bottom-0 mx-auto px-8 w-full flex flex-row justify-between text-left overflow-hidden leading-8">
-                                    Built by Blessed
-                                    <li>Contact Me</li>
-                                </div>
-                            </ul>
-                        </div>
-                    </div>
-                    : <div className="drawer drawer-end">
-                        <input id="desktop-help-drawer" type="checkbox" className="drawer-toggle" />
-                        <div className="drawer-content h-screen overflow-y-auto dark:bg-base-300 z-10">
-                            {/* Page content here */}
-                            {
-                                size?.width <= deviceWidthEnum.desktop
-                                    ? <LaptopNavBar setData={setData} />
-                                    : <DesktopNavBar setData={setData} />
-                            }
-                            <Summary summary={data} />
-                            <Footer />
-                        </div>
-
-                        <div className="drawer-side z-10">
-                            <label htmlFor="desktop-help-drawer" className="drawer-overlay"></label>
-                            <div className="menu relative py-8 w-96 min-h-full overflow-y-auto bg-base-200 text-base-content">
-                                {/* Sidebar content here */}
-                                {/* <li><a>Sidebar Item 1</a></li>
-                                <li><a>Sidebar Item 2</a></li>
-                                <li></li> */}
-                                <HelpContent />
-                                <div className="absolute right-10 top-11 bg-base-300/60 dark:bg-neutral rounded-xl">
-                                    <button className="w-full mx-auto">
-                                        <span class="sr-only">Close</span>
-                                        <label htmlFor="desktop-help-drawer" className="flex justify-center btn btn-ghost items-center w-12 h-8 leading-8 text-center">
-                                            <svg class="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
-                                        </label>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            }
-            {/* {
-                size?.width < deviceWidthEnum.laptop
-                    ? <MobileNavBar />
-                    : <DesktopNavBar setData={setData} />
-            } */}
-            {/* <Summary summary={data} /> */}
-            <Home />
+        <div className="font-sans">
             <Outlet />
             <ScrollRestoration
                 getKey={(location, matches) => {
